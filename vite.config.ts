2f0fa6d3f6e90fbfafbe2bef74ae3f30aadeef5a
Nijat Hamid <nicatorium@gmail.com>
2024-07-15 23:20:11 +0400

    ShadCN added - Folder Structure improved

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import eslintPlugin from "@nabla/vite-plugin-eslint";
-
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +9,10 @@ export default defineConfig({
   server:{
     open:true,
     host:true
-  }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
