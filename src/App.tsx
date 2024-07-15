d12ff26c45cc8d2c4856279a5eef97adf48aa99f
Nijat Hamid <nicatorium@gmail.com>
2024-07-15 22:30:45 +0400

    Vercel Gist Sans font added!

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import './app.css'
-import { env } from './env'
-
 function App() {
-  console.log(env.VITE_string)
-  console.log(env.VITE_number)
-
   return (
     <>
       <h1>Hello World!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './app'
+import '@fontsource/geist-sans/400.css';
+import '@fontsource/geist-sans/500.css';
+import '@fontsource/geist-sans/600.css';
 
 createRoot(document.querySelector('#root')!).render(
   <StrictMode>
