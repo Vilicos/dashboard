684496c0c0f802875cefa62e5012ada72b87f8cc
Nijat Hamid <nicatorium@gmail.com>
2024-07-15 23:47:28 +0400

    ShadCN init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
+import Test from "./pages/test";
+
 function App() {
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+     <Test/>
   );
 }
 
---
+++ src/pages/test.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+function Test() {
+  return (
+   <div>
+     <Button>
+        Hello
+     </Button>
+   </div>
+  )
+}
+
+export default Test
