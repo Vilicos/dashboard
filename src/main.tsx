6704371225e0ef24c06043b2ea469bf265ef8dc4
Nijat Hamid <nicatorium@gmail.com>
2024-07-16 15:31:03 +0400

    Misc fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from '@/app'
+import App from './App';
 import "@styles/index.css"
 import '@fontsource/geist-sans/400.css';
 import '@fontsource/geist-sans/500.css';
