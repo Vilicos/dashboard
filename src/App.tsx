14a06cd2a2bf0dc8fd942dfe5b665d1ddc8fb51f
Nijat Hamid <nicatorium@gmail.com>
2024-07-17 15:10:03 +0400

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import Test from "@pages/test";
-
 function App() {
   return (
-    <Test/>
+    <></>
+    // <div className="fixed left-0 top-0 -z-10 h-full w-full">
+		// 	<div className="relative h-full w-full bg-background">
+		// 		<div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]">
+					
+		// 		</div>
+		// 	</div>
+		// </div>
   );
 }
 
